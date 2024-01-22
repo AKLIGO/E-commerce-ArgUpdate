494cea55c310c660f4201b17e818ee606b2d2b3e
Your AKLIGO <koffitse.akligo@ipnetinstitute.com>
2024-01-22 21:58:11 +0000

    Affichage

--- src/app/revendeur/list-products/list-products.component.ts
+++ src/app/revendeur/list-products/list-products.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+import { ServiceProductService } from '../services/service-product.service';
 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
   styleUrls: ['./list-products.component.css']
 })
-export class ListProductsComponent {
+export class ListProductsComponent implements OnInit {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+  listProduct: Product[] = [];
+  produitService: ServiceProductService;
 
+  constructor(private productService: ServiceProductService) {
+    this.produitService = productService;
+  }
+
+  ngOnInit(): void {
+    this.listProduct = this.produitService.getAllProduits();
+  }
 }
